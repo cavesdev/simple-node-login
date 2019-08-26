1bcf859848a37d64d90690a961c3410478bce4b0
cavesdev <24899540+cavesdev@users.noreply.github.com>
2019-08-26 14:27:42 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// get all the tools we need
 var express = require('express');
 var app =  express();
+
 var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
